bbfd061e51e093bb70bc9989222806b78e074197
MEnglund92 <mattias.englund1992@gmail.com>
2026-02-06 13:45:41 +0100

    Update entrypoint.sh

--- infra-repo/loadbalancer/entrypoint.sh
+++ infra-repo/loadbalancer/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# Aktivera IP-forwarding så att trafik kan skickas vidare genom containern
-echo 1 > /proc/sys/net/ipv4/ip_forward
-
 # Ladda nftables-konfigurationen från filen
 nft -f /etc/nftables.conf
 
@@ -10,5 +7,3 @@ echo "Lastbalanserare startad."
 
 # Håll containern igång (annars dör den direkt efter att reglerna laddats)
 tail -f /dev/null
-
-
